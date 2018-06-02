455953a6a2b2558e8b69bdb1ecb6356252276ae7
Jmeas <jellyes2@gmail.com>
2018-06-02 12:53:42 -0700

    Split up compute-position file

---
+++ src/compute-position/skip-boundary.js
@@ -0,0 +1,9 @@
+export default function skipBoundary({ val, boundaryStart, boundaryEnd, goHigher = true }) {
+  const isBetween = val > boundaryStart && val < boundaryEnd;
+
+  if (isBetween) {
+    return goHigher ? boundaryEnd : boundaryStart;
+  } else {
+    return val;
+  }
+}
